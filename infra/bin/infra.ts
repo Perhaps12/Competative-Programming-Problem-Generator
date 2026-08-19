8e2b66fc6947d8d0c27aad6dce5796054a57d4ae
Perhaps12 <howlongwillthistake4@gmail.com>
2026-08-19 16:51:20 -0400

    CDK infra fixes

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -5,6 +5,10 @@ import { LeetcodeCloneStack } from "../lib/infra-stack";
 const app = new cdk.App();
 
 new LeetcodeCloneStack(app, "LeetcodeCloneStack", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
   // Read from CDK context (see README for how to pass these):
   //   cdk deploy -c keyPairName=your-key-name -c githubRepoUrl=https://github.com/you/repo.git
   keyPairName: app.node.tryGetContext("keyPairName"),
